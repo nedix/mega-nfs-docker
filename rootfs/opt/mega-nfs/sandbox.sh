318ee86fc5b26d8def17c682358862d03391a732
NEDIX <2658411+nedix@users.noreply.github.com>
2024-01-23 01:43:04 +0100

    Fix sandbox

--- rootfs/opt/mega-nfs/sandbox.sh
+++ rootfs/opt/mega-nfs/sandbox.sh
@@ -1,54 +1,51 @@
 #!/usr/bin/env sh
 
-: ${SERVICE_NAME:="$RC_SVCNAME"}
-: ${SERVICE_ID:="${RC_SVCNAME##*-}"}
-: ${CHROOT_PATH:="/chroot/${RC_SVCNAME}"}
+: ${SANDBOX_PATH:="/chroot/${RC_SVCNAME}"}
+: ${SANDBOX_NAME:="$RC_SVCNAME"}
+: ${SANDBOX_NUMBER:="${RC_SVCNAME##*-}"}
 
-[ ! -d "$CHROOT_PATH" ] \
-&& [ -n "$CHROOT_PATH" ] \
-&& [ -n "$SERVICE_ID" ] \
-&& {
-    HOST_VETH="mega${SERVICE_ID}"
-    SERVICE_PORT="1000${SERVICE_ID}"
-    PEER_VETH="veth${SERVICE_ID}"
+[ -n "$SANDBOX_NAME" ] && [ -c "${SANDBOX_PATH}/dev/urandom" ] || {
+    FORWARD_SANDBOX_PORT="1000${SANDBOX_NUMBER}"
+    HOST_VETH="mega${SANDBOX_NUMBER}"
+    PEER_VETH="veth${SANDBOX_NUMBER}"
 
     mkdir -p \
-        "${CHROOT_PATH}/dev" \
-        "${CHROOT_PATH}/home" \
-        "${CHROOT_PATH}/tmp"
+        "${SANDBOX_PATH}/dev" \
+        "${SANDBOX_PATH}/home" \
+        "${SANDBOX_PATH}/tmp"
 
     chown nobody \
-        "${CHROOT_PATH}/home" \
-        "${CHROOT_PATH}/tmp"
+        "${SANDBOX_PATH}/home" \
+        "${SANDBOX_PATH}/tmp"
 
-    mknod -m 0666 "${CHROOT_PATH}/dev/null" c 1 3
-    mknod -m 0666 "${CHROOT_PATH}/dev/urandom" c 1 9
+    mknod -m 0666 "${SANDBOX_PATH}/dev/null" c 1 3
+    mknod -m 0666 "${SANDBOX_PATH}/dev/urandom" c 1 9
 
     # Create a network namespace
-    ip netns add "$SERVICE_NAME"
+    ip netns add "$SANDBOX_NAME"
 
     # Create a veth pair with specified names
     ip link add "$HOST_VETH" type veth peer name "$PEER_VETH"
 
     # Move one end into the network namespace
-    ip link set "$PEER_VETH" netns "$SERVICE_NAME"
+    ip link set "$PEER_VETH" netns "$SANDBOX_NAME"
 
     # Set up the loopback interface within the namespace
-    ip netns exec "$SERVICE_NAME" ip link set lo up
+    ip netns exec "$SANDBOX_NAME" ip link set lo up
 
     # Rename the veth interface to eth0 inside the namespace
-    ip netns exec "$SERVICE_NAME" ip link set "$PEER_VETH" name eth0
+    ip netns exec "$SANDBOX_NAME" ip link set "$PEER_VETH" name eth0
 
     # Assign IP addresses within the namespace
-    ip addr add "10.0.${SERVICE_ID}.1/30" dev "$HOST_VETH"
-    ip netns exec "$SERVICE_NAME" ip addr add "10.0.${SERVICE_ID}.2/30" dev eth0
+    ip addr add "10.0.${SANDBOX_NUMBER}.1/30" dev "$HOST_VETH"
+    ip netns exec "$SANDBOX_NAME" ip addr add "10.0.${SANDBOX_NUMBER}.2/30" dev eth0
 
     # Bring up the veth interfaces
     ip link set "$HOST_VETH" up
-    ip netns exec "$SERVICE_NAME" ip link set eth0 up
+    ip netns exec "$SANDBOX_NAME" ip link set eth0 up
 
     # Set the default route within the namespace
-    ip netns exec "$SERVICE_NAME" ip route add default via "10.0.${SERVICE_ID}.1"
+    ip netns exec "$SANDBOX_NAME" ip route add default via "10.0.${SANDBOX_NUMBER}.1"
 
     # Create the NAT table
     nft add table inet nat
@@ -59,27 +56,16 @@
 
     # Redirect locally generated traffic to the namespace
     nft add chain inet nat output \{ type nat hook output priority 0 \; \}
-    nft add rule inet nat output ip daddr 127.0.0.1 tcp dport "$SERVICE_PORT" dnat to "10.0.${SERVICE_ID}.2:${SERVICE_PORT}"
+    nft add rule inet nat output ip daddr 127.0.0.1 tcp dport "$FORWARD_SANDBOX_PORT" dnat to "10.0.${SANDBOX_NUMBER}.2:${FORWARD_SANDBOX_PORT}"
 }
 
-jail() {
+sandbox() {
     CMD="$@"
 
-    su -s /bin/sh -c " \
+    ip netns exec ${SANDBOX_NAME} su -s /bin/sh -c " \
         HOME=/home \
         unshare -r \
-        chroot ${CHROOT_PATH} \
-        ${CMD}
-    " nobody
-}
-
-firewall() {
-    CMD="$@"
-
-    ip netns exec ${SERVICE_NAME} su -s /bin/sh -c " \
-        HOME=/home \
-        unshare -r \
-        chroot ${CHROOT_PATH} \
+        chroot ${SANDBOX_PATH} \
         ${CMD}
     " nobody
 }
